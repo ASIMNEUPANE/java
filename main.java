6148b65fd18bfa26d72aa7bf2f53e05a37fdaa79
ASIMNEUPANE <asimneupane11@gmail.com>
2024-06-30 23:17:00 +0545

    Add Shape and Triangle classes

---
+++ main.java
@@ -0,0 +1,83 @@
+// class Pen {
+//     String color;
+
+//     public  void write(int a){
+//         System.out.println("Hello" + a);
+
+
+//     }
+//     public void printCOlor(){
+//         System.out.println(color);
+//     }
+// }
+
+// class Student{
+//     String name;
+//     int age;
+//     String id;
+    
+//    public void listData(){
+//         System.out.println(this.name + "id" + this.age + "years old");
+//     }
+
+//     Student(){
+//         System.out.println("constructor ")
+//     }
+// }
+
+
+// public class main {
+
+//     // public static void x(String args[]){
+//     //     Pen asimPen = new Pen();
+//     //     asimPen.color = "blue";
+        
+//     //     Pen asimPen2 = new Pen(); 
+//     //     asimPen2.color = "black";
+//     //     asimPen.printCOlor();
+//     //     asimPen2.printCOlor();
+//     // }
+
+//     public static void main(String args[]){
+//         Student Asim = new Student();
+
+//         Asim.age = 15;
+//         Asim.id="jisdikj321";
+//         Asim.name = "asim";
+//         Asim.listData();
+
+//     }
+// }
+
+// // constructor name should be same as class
+
+
+class Shape {
+    String color;
+}
+
+class Traingle extends Shape{
+
+    
+}
+
+public class main{
+
+public  void main(String args[]){
+Traingle t1 = new Traingle();
+t1.color = "bliue";
+
+}
+
+}
+
+    
+
+
+
+
+
+
+
+
+
