dc9594b08c4ca08425a7fa207c83108464c8e566
ASIMNEUPANE <asimneupane11@gmail.com>
2024-07-01 23:26:21 +0545

    java

---
+++ Main.java
@@ -0,0 +1,21 @@
+
+
+
+public class Main {
+    
+public static void main(String[] args){
+double myDouble = 9.78d;
+int myInt = 
+(int)
+ myDouble;
+     System.out.println(myDouble);
+
+    System.out.println(myInt);
+}
+
+}
+
+
+
+
+
