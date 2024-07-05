f03274398302107c5000e8a91a7a747a91095c4b
ASIMNEUPANE <asimneupane11@gmail.com>
2024-07-05 11:50:22 +0545

    interface

--- Interface.java
+++ Interface.java
@@ -4,22 +4,26 @@ interface Man {
   public void sleep(); // interface method (does not have a body)
 }
 
+interface Second{
+  public void aa()
+}
+
 // Pig "implements" the Animal interface
-class Pig implements Man {
+class Pig implements Man, Second {  //multiple interdace
   public void animalSound() {
     // The body of animalSound() is provided here
     System.out.println("The pig says: wee wee");
   }
-  public void sleep() {
-    // The body of sleep() is provided here
-    System.out.println("Zzz");
-  }
+  // public void sleep() {
+  //   // The body of sleep() is provided here
+  //   System.out.println("Zzz");
+  // }
 }
 
 class Interface {
   public static void main(String[] args) {
     Pig myPig = new Pig();  // Create a Pig object
     myPig.animalSound();
-    myPig.sleep();
+    // myPig.sleep();
   }
 }
